795c485de53d5f32b32a9d18228947759a85d4b0
XadillaX <admin@xcoder.in>
2014-01-17 19:18:00 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+/**
+ * [ ❤ Spidex ❤ ]
+ * Created by XadillaX on 14-1-17.
+ */
+require("sugar/release/sugar-full.development");
+var Spidex = require("./lib/spidex");
+var util = require("util");
+var statics = require("./lib/statics");
+
+module.exports = new Spidex();
+
+/**
+ * get the default user-agent
+ * @returns {userAgent|*}
+ */
+module.exports.getDefaultUserAgent = function() {
+    return statics.userAgent;
+};
+
+/**
+ * set the default user-agent
+ * @param userAgent
+ */
+module.exports.setDefaultUserAgent = function(userAgent) {
+    statics.userAgent = userAgent;
+};
+
+/**
+ * parse Cookie.
+ * @param respHeader
+ * @returns {string}
+ */
+module.exports.parseCookie = function(respHeader) {
+    var cookies = respHeader["set-cookie"];
+    if(!cookies || !util.isArray(cookies) || cookies.length === 0) {
+        return "";
+    }
+
+    var cookie = "";
+    for(var i = 0; i < cookies.length; i++) {
+        var tmpCookie = cookies[i];
+        if(tmpCookie.indexOf(";") !== -1) {
+            tmpCookie = tmpCookie.substr(0, tmpCookie.indexOf(";") + 1);
+        }
+        cookie += tmpCookie;
+        cookie += " ";
+    }
+
+    return cookie;
+};
---
+++ tester/tester.js
@@ -0,0 +1,20 @@
+/**
+ * Created by XadillaX on 14-1-17.
+ */
+var spidex = require("../");
+
+spidex.setDefaultUserAgent("Spidex v1.0 (node.js client)");
+spidex.post("http://cnodejs.org/signin", function(html, status, respHeader) {
+    var header = {
+        cookie      : spidex.parseCookie(respHeader)
+    };
+    spidex.get("http://cnodejs.org/", function(html) {
+        console.log(html);
+    }, header, "utf8");
+}, {
+    name    : "yourusername",
+    pass    : "yourpassword",
+    csrf    : "undefined"
+}).on("err", function(err) {
+    console.log(err);
+});
